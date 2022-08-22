aa080886d9d9786b81249f42ea153339196b9d64
Brice Daupiard <brice.daupiard@nowbrains.com>
2022-08-22 18:16:29 +0200

    fix: translation

--- lib/core-data-table/app-date.pipe.d.ts
+++ lib/core-data-table/app-date.pipe.d.ts
@@ -5,7 +5,7 @@ export declare class DateFormatConstants {
     static readonly dateLocalFr = "fr-FR";
     static readonly dateLocalEn = "en-EN";
     static readonly timezone = "GMT+2";
-    static readonly DATE_FMT_FR = "dd/MM/y";
+    static readonly DATE_FMT_FR = "dd/MM/yyyy";
     static readonly DATE_TIME_FMT_FR: string;
     static readonly DATE_FMT_EN = "MM/dd/y";
     static readonly DATE_TIME_FMT_EN: string;
@@ -15,8 +15,7 @@ export declare enum AppLanguages {
     En = "en"
 }
 export declare class AppDatePipe extends DatePipe implements PipeTransform {
-    showTime?: boolean;
-    transform(value: any, lang?: any): any;
+    transform(value: any, lang?: any, showDate?: boolean): any;
     static ɵfac: i0.ɵɵFactoryDeclaration<AppDatePipe, never>;
     static ɵpipe: i0.ɵɵPipeDeclaration<AppDatePipe, "appDate">;
 }
