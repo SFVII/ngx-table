4d1378baa7a919e987d842a13fbefad243183430
kevin violas <kevin.violas@nowbrains.com>
2022-09-30 10:36:07 +0200

    fix: test

--- fesm2015/ngx-table.mjs
+++ fesm2015/ngx-table.mjs
@@ -2190,11 +2190,13 @@ class TableComponent {
             && this.inputSearch.length < 200) {
             if (this.data) {
                 this.data.filter(this.inputSearch);
-                console.log('next 0 in input search');
-                this.data.pageNumber.next(0);
                 this.data.fetch(0);
                 this.data.number = 0;
                 this.changeDetectorRef.markForCheck();
+            }
+            if (this.inputSearch.length > 0) {
+                console.log('next 0 in input search');
+                this.data.pageNumber.next(0);
             }
         }
         //  this.ngOnDestroy();
